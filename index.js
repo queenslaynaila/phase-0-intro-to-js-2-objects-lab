ee9a5d5ac259ada49d4910951c76d4d005ce92a4
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-04 02:19:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var employee = {
+    name: "jema",
+    streetAddress: "nairobi"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newObject = { ...employee }
+    newObject[key] = value;
+    return newObject
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    let secondObject = { ...employee };
+    delete secondObject[key]
+    return secondObject
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let thirdObject = {...employee}
+    delete employee[key]
+    return employee
+    
+}
